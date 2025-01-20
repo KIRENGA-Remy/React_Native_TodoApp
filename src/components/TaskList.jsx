d1894da86c59393fcc84ef1c81429bffed99f471
GITOLI Remy Claudien <gitoliremy@gmail.com>
2025-01-19 22:07:26 -0700

    button addition

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,33 @@
-import { View, Text, TouchableWithoutFeedback, Keyboard, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, TouchableWithoutFeedback, Keyboard, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
 export default function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
     return(
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={styles.container}>
                 <Text style={styles.title}>Login</Text>
+                <TextInput 
+                placeholder="Enter email"
+                placeholderTextColor={'#aaa'}
+                keyboardType="email-address"
+                style={styles.input}
+                value={email}
+                />
+                <TextInput 
+                placeholder="Enter password"
+                placeholderTextColor={'gray'}
+                value={password}
+                secureTextEntry
+                style={styles.input}
+                />
+                <TouchableOpacity>
+                    <view>Login</view>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View>Register</View>
+                </TouchableOpacity>
             </View>
         </TouchableWithoutFeedback>
     )
@@ -22,5 +45,13 @@ const styles = StyleSheet.create({
         fontSize: 12,
         color: '#fff',
         alignItems: 'center'
+    },
+    input: {
+        borderRadius: 5,
+        borderWidth: 1,
+        backgroundColor: 'gray',
+        borderColor: 'Black',
+        padding: 8,
+        marginTop: 12
     }
 })
